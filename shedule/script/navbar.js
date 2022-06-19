f19e3cbe5f7237d2843b286b0f9f43d0e82876bd
Dinesh Kumar <dinesh.jangid323@gmail.com>
2022-06-19 19:07:37 +0530

    page linking

--- shedule/script/navbar.js
+++ shedule/script/navbar.js
@@ -1,107 +1,96 @@
-let usernav = document.querySelector("#signup>a+div")
+let usernav = document.querySelector("#signup>a+div");
 let signuppagelink = document.querySelector("#signup>a");
-
-
 
 let userstatus = JSON.parse(localStorage.getItem("cickuserlogin"));
 
-if(userstatus!==true){
-    signuppagelink.setAttribute("href", "signup.html")
-}else{
-    signuppagelink.setAttribute("href", "reguser.html");
-    let div = document.createElement("div");
-    let p = document.createElement("p");
-    let p2 = document.createElement("p");
-    
-    let accountlink = document.createElement("a");
-    accountlink.setAttribute("href", "home.html")
-    accountlink.innerText = "My Account";
-    p.append(accountlink);
+if (userstatus !== true) {
+  signuppagelink.setAttribute("href", "signup.html");
+} else {
+  signuppagelink.setAttribute("href", "/home.html");
+  let div = document.createElement("div");
+  let p = document.createElement("p");
+  let p2 = document.createElement("p");
 
-    let signoutlink = document.createElement("a");
-    signoutlink.addEventListener("click", signout);
-    signoutlink.innerText="Sign out";
-    p2.append(signoutlink);
+  let accountlink = document.createElement("a");
+  accountlink.setAttribute("href", "home.html");
+  accountlink.innerText = "My Account";
+  p.append(accountlink);
 
-    div.append(p, p2);
-    div.setAttribute("class", "regusernav")
-    usernav.append(div);
+  let signoutlink = document.createElement("a");
+  signoutlink.addEventListener("click", signout);
+  signoutlink.innerText = "Sign out";
+  p2.append(signoutlink);
+
+  div.append(p, p2);
+  div.setAttribute("class", "regusernav");
+  usernav.append(div);
 }
 
-function signout(){
-    localStorage.removeItem("cickuserlogin");
-    localStorage.removeItem("cickbuzzuser");
-    window.location.href = "signup.html";
+function signout() {
+  localStorage.removeItem("cickuserlogin");
+  localStorage.removeItem("cickbuzzuser");
+  window.location.href = "/home.html";
 }
-
-
 
 // Shedule Page navbar
 
-
-
 let submenus = [
-    {
-        name:"Current Matches",
-        url:"currentmath"
-    },
-    {
-        name:"Current & Future Series",
-        url:"currentandfuture"
-    },
-    {
-        name:"Matches By Day",
-        url:"matchdaybyday"
-    },{
-        name:"Teams",
-        url:"teams"
-    },
-    {
-        name:"Series Archive",
-        url:"series"
-    }
+  {
+    name: "Current Matches",
+    url: "currentmath",
+  },
+  {
+    name: "Current & Future Series",
+    url: "currentandfuture",
+  },
+  {
+    name: "Matches By Day",
+    url: "matchdaybyday",
+  },
+  {
+    name: "Teams",
+    url: "teams",
+  },
+  {
+    name: "Series Archive",
+    url: "series",
+  },
 ];
 
-let submenu =document.getElementById("submenu");
-submenus.forEach(function(el, i){
-    let a = document.createElement("a");
-    let display = document.getElementById(el.url);
-    a.innerText = el.name;
-    if(el.name=="Matches By Day"){
-        a.setAttribute("class","selected");
-        
-        display.setAttribute("class", "show");
-    }else{
-        display.setAttribute("class", "hide");
-    }
-    a.addEventListener("click", function(){
-        loadPage(el);
-    })
-    submenu.append(a);
+let submenu = document.getElementById("submenu");
+submenus.forEach(function (el, i) {
+  let a = document.createElement("a");
+  let display = document.getElementById(el.url);
+  a.innerText = el.name;
+  if (el.name == "Matches By Day") {
+    a.setAttribute("class", "selected");
 
-})
-
-function loadPage(el){
-    let load = document.getElementById("pageLoadSection");
-    let selectedMune = document.querySelectorAll("#submenu>a");
-    let display = document.getElementById(el.url);
-    let  hide = document.querySelector(".show");
-
-    selectedMune.forEach(function(el1){
-
-        if(el1.innerText==el.name){
-            el1.setAttribute("class", "selected");
-        }else{
-
-            el1.removeAttribute("class", "selected");
-
-        }
-    })
-    
-    display.removeAttribute("class", "hide");
     display.setAttribute("class", "show");
-    hide.removeAttribute("class", "show");
-    hide.setAttribute("class", "hide");
+  } else {
+    display.setAttribute("class", "hide");
+  }
+  a.addEventListener("click", function () {
+    loadPage(el);
+  });
+  submenu.append(a);
+});
 
-    
+function loadPage(el) {
+  let load = document.getElementById("pageLoadSection");
+  let selectedMune = document.querySelectorAll("#submenu>a");
+  let display = document.getElementById(el.url);
+  let hide = document.querySelector(".show");
+
+  selectedMune.forEach(function (el1) {
+    if (el1.innerText == el.name) {
+      el1.setAttribute("class", "selected");
+    } else {
+      el1.removeAttribute("class", "selected");
+    }
+  });
+
+  display.removeAttribute("class", "hide");
+  display.setAttribute("class", "show");
+  hide.removeAttribute("class", "show");
+  hide.setAttribute("class", "hide");
 }
